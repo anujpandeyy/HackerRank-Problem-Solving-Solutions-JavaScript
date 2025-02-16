583945ca4c7d70d11d20f8717af8ab76267a38eb
Anuj Pandey <anujpandey1701@yahoo.com>
2025-02-16 17:07:00 +0530

    Add betweenTwoSets

---
+++ betweenTwoSets.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function getTotalX(a, b) {
+    let validCount = 0;
+    for (let x = 1; x <= 100; x++) {
+        if (a.every(int => (x % int == 0))) {
+            if (b.every(int => (int % x == 0))) {
+                validCount++;
+            }
+        }
+    }
+    return validCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
